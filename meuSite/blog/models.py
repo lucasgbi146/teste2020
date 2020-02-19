ebdf349ec904ee54191ca921af4af6112811ea23
Lucas Santos <lucasgbi146@gmail.com>
2020-02-19 09:35:31 -0300

    atualizacao dos modelos

--- meuSite/blog/models.py
+++ meuSite/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField('Título', max_length=200)
+    texto = models.TextField('Conteúdo')
+    autor = models.ForeignKey('User', on_delete=models.CASCADE)
+    Data_pub = models.DateTimeField('Data de Publicação', auto_now())
+    data_alt = nodels.DateTimeField('Data de Atualização', add_auto_now()) 
+    imagem = models.ImageField('Imagem', upload_to='imagens')
